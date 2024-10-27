c9ce3b86da3f07934d3fcb4e8b642c35edd72e1e
Jefferson Moraes <jeffersonjuniormoraes@gmail.com>
2024-10-27 16:01:49 -0300

    2610 - Valor Médio dos Produtos

--- SQL/2609 - Produtos por Categorias.sql
+++ SQL/2609 - Produtos por Categorias.sql
@@ -1,6 +1,8 @@
-SELECT 
+SELECT
     c.name,
     sum(p.amount)
 FROM
     categories c
-INNER JOIN products p ON c.id = p.id_categories
+    INNER JOIN products p ON c.id = p.id_categories
+GROUP BY
+    c.name
